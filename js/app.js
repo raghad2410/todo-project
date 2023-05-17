0f856c1ebbb309c371097f9d66d766321a4d39ea
raghad2410 <raghadnarmoq@gmail.com>
2023-05-17 06:21:04 +0000

    JavaScript Assignment

--- js/app.js
+++ js/app.js
@@ -1,59 +1,65 @@
-// let userName = prompt("Please Enter Your Name:");
+let userName = prompt("Please Enter Your Name:");
 
 
-// let age = prompt("Please Enter Your Age:");
-// if (age <= 0) {
-//     alert("your age should not be zero or less");
-// } else {
-//     alert(`your age is ${age}`)
-// }
+let age = prompt("Please Enter Your Age:");
+if (age <= 0) {
+    alert("your age should not be zero or less");
+}
 
 
-// let gender = prompt("Please Enter Your Gender: (male or female)").toLocaleLowerCase();
-// let gen = 0;
+let gender = prompt("Please Enter Your Gender: (male or female)").toLocaleLowerCase();
+let gen = 0;
 
-// if (gender === "male") {
-//     gen = "Mr";
-// } else if (gender === "female") {
-//     gen = "Ms";
-// } else {
-//     gen = "";
-// }
-
-// let arrayAnswer = [];
-
-// function theQues(quesYesNo) {
-//     answers = prompt(quesYesNo)
-
-//     function array(arrayAns) {
-//         if (arrayAns.toLocaleLowerCase() === "yes") {
-//             arrayAnswer.push("Yes")
-//         } else if (arrayAns.toLocaleLowerCase() === "no") {
-//             arrayAnswer.push("No")
-//         } else {
-//             arrayAnswer.push("Invalid")
-//         }
-//         return arrayAns
-//     }
-//     array(answers)
-
-//     return (theQues)
-// }
-// console.log(arrayAnswer)
-// theQues("Are You A Front-End Developer?");
-// theQues("Are You A Back-End Developer?");
-// theQues("Are You Familiar With JavaScript Language?");
+if (gender === "male") {
+    gen = "Mr ";
+} else if (gender === "female") {
+    gen = "Ms ";
+} else {
+    gen = "";
+}
 
 
+let welcome = prompt("Are You Want to skip the welcoming message?")
+if (welcome.toLocaleLowerCase() === "yes") {
+    alert(`welcome ${gen}${userName}`)
+}
 
 
-// function wholeArray(wholeArr) {
-//     for (i = 0; i < wholeArr.length; i++) {
-//         console.log(wholeArr[i]);
-//     }
-//     return wholeArr
-// }
-// wholeArray(arrayAnswer)
+let arrayAnswer = [];
+
+function theQues(quesYesNo) {
+    answers = prompt(quesYesNo)
+
+    function array(arrayAns) {
+        if (arrayAns.toLocaleLowerCase() === "yes") {
+            arrayAnswer.push("Yes")
+        } else if (arrayAns.toLocaleLowerCase() === "no") {
+            arrayAnswer.push("No")
+        } else {
+            arrayAnswer.push("Invalid")
+        }
+        return arrayAns
+    }
+    array(answers)
+
+    return (theQues)
+}
+console.log(arrayAnswer)
+theQues("Are You A Front-End Developer?");
+theQues("Are You A Back-End Developer?");
+theQues("Are You Familiar With JavaScript Language?");
+
+
+
+
+function wholeArray(wholeArr) {
+    for (i = 0; i < wholeArr.length; i++) {
+        console.log(wholeArr[i]);
+    }
+    return wholeArr
+}
+wholeArray(arrayAnswer)
+
 
 
 
